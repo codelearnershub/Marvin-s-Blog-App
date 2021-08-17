466d45d31f8d8bb51ae8c23e2b71546fa5a32567
Marvellous <adeoyemarvellous7@gmail.com>
2021-08-17 11:32:39 +0100

    initial commit

---
+++ MarvinBlogv.2.0/Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System;
+namespace MarvinBlogv.2.0.Models
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedAt {get;set;}
+
+        public int Id {get;set;}
+    }
+}
---
+++ MarvinBlogv.2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MarvinBlogv._2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
