73a7935704590e041eb2bd93e393ee06aad81d8d
Marvellous <adeoyemarvellous7@gmail.com>
2021-08-19 16:45:52 +0100

    Modified Implementation Of Repositories

---
+++ MarvinBlogv.2.0/Interfaces/IPostCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using MarvinBlogv._2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvinBlogv._2._0.Interfaces
+{
+    public interface IPostCategoryRepository
+    {
+        public PostCategory AddPostCategory(PostCategory postCategory);
+        public void Delete(int id);
+        public PostCategory UpdatePostCategory(PostCategory postCategory);
+        public PostCategory FindPostCategory(int id);
+    }
+}
